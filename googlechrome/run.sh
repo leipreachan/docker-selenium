d58603e99b6152ec0997a5d1e46ec1b36ecca570
Artem Soldatkin <a.soldatkin@corp.badoo.com>
2014-10-03 10:55:29 +0400

    remove orphan containers on restart

--- googlechrome/run.sh
+++ googlechrome/run.sh
@@ -1,1 +1,1 @@
-docker run --net=host -i -p 5900:5900 -p 4501:4501 -v /home/selenium/PhotosForTests:/home/selenium/PhotosForTests -v /var/log/docker-selenium:/var/log/selenium -t selenium/googlechrome
+docker run --rm --net=host -i -p 5901:5900 -p 4502:4501 -v /home/selenium/PhotosForTests:/home/selenium/PhotosForTests -v /var/log/docker-selenium:/var/log/selenium -t selenium/googlechrome
