6281f30c30fafdfea6ef305bce0722b1d54fac3b
Artem Soldatkin <a.soldatkin@corp.badoo.com>
2014-10-03 18:12:53 +0400

    fixed macos xargs parameters

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 browsers=( "cd firefox && ./build.sh" "cd googlechrome && ./build.sh" "cd grid && ./build.sh" )
 size=${#browsers[@]}
 
-cd ubuntu && ./build.sh && cd ../ && printf '%s\n' "${browsers[@]}" | xargs -P $size -i -t sh -c "{}"
+cd ubuntu && ./build.sh && cd ../ && printf '%s\n' "${browsers[@]}" | xargs -P $size -I{} -t sh -c "{}"
