e4935a78021a985b1e7e761c812bdd138bb5a31f
Artem Soldatkin <a.soldatkin@corp.badoo.com>
2014-10-07 17:59:04 +0400

    check that boot2docker installed in macos

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -n "$(bash --version | grep apple)" ]; then
+	if [ -z "$(which boot2docker)" ]; then
+		echo "Please, install boot2docker from this page: https://github.com/boot2docker/osx-installer/releases then restart this script"
+		exit 1
+	fi
+fi
+
 browsers=( "cd firefox && ./build.sh" "cd googlechrome && ./build.sh" "cd grid && ./build.sh" )
 size=${#browsers[@]}
 
