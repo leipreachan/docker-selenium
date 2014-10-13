f0c82a91bb0e86c75078c2153780f91e5510643a
Artem Soldatkin <a.soldatkin@corp.badoo.com>
2014-10-13 12:59:00 +0400

    use local ip for grid hub instead mine :)

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-NUMBER_OF_NODES=5
+NUMBER_OF_NODES=2
 
 if [ -n "$1" ]; then
 	NUMBER_OF_NODES=$1
 fi
 
+LOCAL_IP=$((/sbin/ifconfig | egrep "inet addr" | grep "192" | sed 's/:/ /g' | awk '{print $3}'))
 cd grid && ./run.sh 5555
-cd ../firefox && ./run.sh 4501 5900 192.168.3.38:5555 $NUMBER_OF_NODES
-cd ../googlechrome && ./run.sh 4551 5951 192.168.3.38:5555 $NUMBER_OF_NODES
+cd ../firefox && ./run.sh 4501 5900 $LOCAL_IP:5555 $NUMBER_OF_NODES
+cd ../googlechrome && ./run.sh 4551 5951 $LOCAL_IP:5555 $NUMBER_OF_NODES
