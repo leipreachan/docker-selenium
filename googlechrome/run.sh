8f0bd3e2d53fd78c6d4fdfbd6dc281ac7fe87a8d
Artem Soldatkin <a.soldatkin@corp.badoo.com>
2014-10-13 12:25:18 +0400

    now you can set number of firefox and chrome nodes to run.sh

--- googlechrome/run.sh
+++ googlechrome/run.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 source ../bin/settings.ini
 
-docker run -d -i $HUB_ADDRESS -p $VNC_PORT:$VNC_PORT -p $SELENIUM_PORT:$SELENIUM_PORT -e VNC_PORT=$VNC_PORT -e SELENIUM_PORT=$SELENIUM_PORT $SHARES --name="selenium-chrome-$SELENIUM_PORT-$VNC_PORT"  -t selenium/googlechrome | tee ../googlechrome-${SELENIUM_PORT}.pid
+function _run()
+{
+	name="googlechrome"
+	docker run -d -i $HUB_ADDRESS -p $VNC_PORT:$VNC_PORT -p $SELENIUM_PORT:$SELENIUM_PORT -e VNC_PORT=$VNC_PORT -e SELENIUM_PORT=$SELENIUM_PORT $SHARES --name="selenium-${name}-$SELENIUM_PORT-$VNC_PORT"  -t "selenium/${name}" | tee ../${name}-${SELENIUM_PORT}.pid
+
+}
+
+_run 
+
+
+if [ -n "$NUMBER_OF_NODES" ]; then
+	for (( i=1; i<$NUMBER_OF_NODES; i++ )); do
+		SELENIUM_PORT=$((SELENIUM_PORT+i))
+		VNC_PORT=$((VNC_PORT+i))
+		_run 
+	done
+fi
--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+NUMBER_OF_NODES=5
+
+if [ -n "$1" ]; then
+	NUMBER_OF_NODES=$1
+fi
+
 cd grid && ./run.sh 5555
-cd ../firefox && ./run.sh 4501 5900 localhost:5555
-cd ../googlechrome && ./run.sh 4551 5951 localhost:5555
+cd ../firefox && ./run.sh 4501 5900 192.168.3.38:5555 $NUMBER_OF_NODES
+cd ../googlechrome && ./run.sh 4551 5951 192.168.3.38:5555 $NUMBER_OF_NODES
