d6b1c3204058d5bd9f432f796e92910838b490da
Artem Soldatkin <a.soldatkin@corp.badoo.com>
2014-10-13 18:48:52 +0400

    more universal IP resolving: MacOS and Linux

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ if [ -n "$1" ]; then
 	NUMBER_OF_NODES=$1
 fi
 
-LOCAL_IP=$(/sbin/ifconfig | egrep 'inet addr' | grep '192' | sed 's/:/ /g' | awk '{print $3}')
+LOCAL_IP=$(/sbin/ifconfig | egrep 'inet.*192' | sed 's/addr:/ /g' | awk '{print $2}')
 cd grid && ./run.sh 5555
 cd ../firefox && ./run.sh 4501 5900 $LOCAL_IP:5555 $NUMBER_OF_NODES
 cd ../googlechrome && ./run.sh 4551 5951 $LOCAL_IP:5555 $NUMBER_OF_NODES
